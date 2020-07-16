79521ee2b56c2ba4d0cd8b7975f414b065068f5c
chantebryana <chantebryana@gmail.com>
2020-07-16 16:12:28 -0500

    Udemy section 3 -- video 36

---
+++ components/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail avatar={faker.image.avatar()} author="Sam" time="Today at 18:00" comment="Nice blog post!" />
+            <CommentDetail avatar={faker.image.avatar()} author="Alex" time="Today at 13:15" comment="Great thoughts" />
+            <CommentDetail avatar={faker.image.avatar()} author="Janel" time="Today at 11:08" comment="Everything is wacky." />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
